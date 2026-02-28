f60938554862469cf69c7de117a98c4a317a218e
alkahtg2 <ghadahaalkahtani@gmail.com>
2026-02-28 20:26:07 +0000

    alphabet in lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+'a' → إلى → 'z'
+✅ الحل الصحيح
+#include <stdio.h>
+
+int main(void)
+{
+    char ch = 'a';
+
+    while (ch <= 'z')
+    {
+        putchar(ch);
+        ch++;
+    }
+
+    putchar('\n');
+
+    return (0);
+}
