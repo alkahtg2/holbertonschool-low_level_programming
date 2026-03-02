1c4e147d350629deec7a3f70cb5dbc4afd9553f8
alkahtg2 <ghadahaalkahtani@gmail.com>
2026-03-02 22:11:00 +0000

    added comment in putchar function in 3-print_numvers.c file

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,11 @@
 #include "main.h"
 
-/**
- * print_numbers - prints the numbers from 0 to 9
- * Return: void
- */
 void print_numbers(void)
 {
-	char c;
+    char c;
 
-	for (c = '0'; c <= '9'; c++)
-		_putchar(c);
+    for (c = '0'; c <= '9'; c++)
+        _putchar(c);
 
-	_putchar('\n');  // new line at the end
+    _putchar('\n'); /* new line at the end */
 }
