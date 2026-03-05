114dd1f7680997e891a3a228ffa9a91caad5352b
alkahtg2 <ghadahaalkahtani@gmail.com>
2026-03-05 21:00:43 +0000

    update command

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * reset_to_98 - takes a pointer to an int and updates the value it points to 98
@@ -6,19 +6,5 @@
  */
 void reset_to_98(int *n)
 {
-    *n = 98; // dereference the pointer and set the value to 98
-}
-
-int main(void)
-{
-    int n;
-
-    n = 0; // any initial value
-    printf("Before reset: n = %d\n", n);
-
-    reset_to_98(&n); // pass the address of n to the function
-
-    printf("After reset: n = %d\n", n);
-
-    return 0;
+    *n = 98; /* dereference the pointer and set the value to 98 */
 }
