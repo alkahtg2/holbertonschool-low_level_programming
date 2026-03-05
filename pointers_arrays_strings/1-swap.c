2cc7c697610c8936b27c13d283b091d7752e6c76
alkahtg2 <ghadahaalkahtani@gmail.com>
2026-03-05 21:07:54 +0000

    add comand to swap in two integrs

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer to the first integer
+ * @b: pointer to the second integer
+ */
+void swap_int(int *a, int *b)
+{
+    int temp;
+
+    temp = *a;  /* store the value of a */
+    *a = *b;    /* assign value of b to a */
+    *b = temp;  /* assign the stored value to b */
+}
