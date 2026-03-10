bff4e7d026beef6a7eb3dcade1d74748b5f315c1
alkahtg2 <ghadahaalkahtani@gmail.com>
2026-03-10 12:16:07 +0000

    add function that searches a string for any of a set of bytes

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h> /* for NULL */
+
+/**
+* _strpbrk - searches a string for any of a set of bytes
+* @s: pointer to the string to search
+* @accept: pointer to the string containing bytes to match
+*
+* Return: pointer to the first byte in s that matches a byte in accept
+*         or NULL if no byte matches
+*/
+char *_strpbrk(char *s, char *accept)
+{
+unsigned int i, j;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+return (&s[i]);
+}
+}
+
+return (NULL); /* no matching byte found */
+}
