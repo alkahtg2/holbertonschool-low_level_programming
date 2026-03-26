f04525b022f05e05c733ad626c6e20edd378989e
alkahtg2 <ghadahaalkahtani@gmail.com>
2026-03-26 22:04:03 +0000

    fix the betty in file name 0-strcar.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,25 +2,25 @@
 
 char *_strcat(char *dest, char *src)
 {
-    int i = 0;
-    int j = 0;
+int i = 0;
+int j = 0;
 
-    /* move to end of dest */
-    while (dest[i] != '\0')
-    {
-        i++;
-    }
+/* move to end of dest */
+while (dest[i] != '\0')
+{
+i++;
+}
 
-    /* append src */
-    while (src[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
+/* append src */
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
 
-    /* null terminate */
-    dest[i] = '\0';
+/* null terminate */
+dest[i] = '\0';
 
-    return (dest);
+return (dest);
 }
