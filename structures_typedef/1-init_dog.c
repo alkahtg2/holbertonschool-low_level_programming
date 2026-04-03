deef30bc20657c40be2b1e3680b2f7387fc77b72
alkahtg2 <ghadahaalkahtani@gmail.com>
2026-04-03 22:24:25 +0000

    fix codes

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,21 +1,22 @@
 #include "dog.h"
+#include <stddef.h>
 
 /**
- * init_dog - initializes a variable of type struct dog
- * @d: pointer to the struct dog to initialize
- * @name: name to assign
- * @age: age to assign
- * @owner: owner to assign
+ * init_dog - initializes a struct dog
+ * @d: pointer to the struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  *
- * Description: Assigns values to the members of a struct dog
- *              through a pointer. Does nothing if d is NULL.
+ * Description: assigns values to struct dog members
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    if (d == NULL)
-        return;
+	if (d == NULL)
+		return;
 
-    d->name = name;
-    d->age = age;
-    d->owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
