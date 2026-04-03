9011989b2e8a87ef0e56e939cb8d238c5f74eb64
alkahtg2 <ghadahaalkahtani@gmail.com>
2026-04-03 22:26:35 +0000

    fix file header

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -1,27 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - Structure representing a dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
-/**
- * init_dog - initializes a struct dog
- * @d: pointer to the struct dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- */
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
