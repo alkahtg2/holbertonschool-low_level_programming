de25816d64f782a28dc262911d3abe2514d0e43d
alkahtg2 <ghadahaalkahtani@gmail.com>
2026-04-03 22:50:31 +0000

    add code

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,8 +8,10 @@ struct dog
 	char *owner;
 };
 
-void print_dog(struct dog *d);
 typedef struct dog dog_t;
 
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
